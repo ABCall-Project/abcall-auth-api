ef8dffe77a66322ee675b55385904eb0ed49bb99
migueltovar <loganitsolutions@outlook.com>
2024-11-14 01:09:35 -0500

    adding test

--- tests/AuthUserEndpointTest.py
+++ tests/AuthUserEndpointTest.py
@@ -22,16 +22,23 @@ class TestAuthUserView(unittest.TestCase):
 
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
 
-    # @patch.object(AuthService, 'list_users_by_customer')
-    # def test_get_users_by_customer_success(self, mock_list_users_by_customer):
-    #     mock_list_users_by_customer.return_value = [
-    #         MagicMock(to_dict=lambda: {"id": "1", "name": "User1"}),
-    #     ]
-    #     auth_user_view = AuthUser()
+    @patch.object(AuthUser, '__init__', lambda x: None)  # Mockea el constructor de AuthUser para evitar la conexión a la base de datos
+    @patch.object(AuthService, 'list_users_by_customer')
+    @patch('flaskr.endpoint.AuthUser.AuthCustomerPostgresqlRepository', autospec=True)  
+    @patch('flaskr.endpoint.AuthUser.AuthPostgresqlRepository', autospec=True) 
+    def test_get_users_by_customer_success(self, mock_auth_repository, mock_auth_user_customer_repository, mock_list_users_by_customer):
+        mock_list_users_by_customer.return_value = [
+            MagicMock(to_dict=lambda: {"id": "1", "name": "User1"}),
+        ]
+        
+        
+        auth_user_view = AuthUser()
+        auth_user_view.service = MagicMock()  
 
-    #     with app.test_request_context('/auth_user?action=getUsersByCustomer', method='GET', query_string={'customer_id': '1'}):
-    #         response = auth_user_view.get(action='getUsersByCustomer')
-    #         self.assertEqual(response[1], HTTPStatus.OK)
+        
+        with app.test_request_context('/auth_user/getUsersByCustomer', method='GET', query_string={'customer_id': '1'}):
+            response = auth_user_view.get(action='getUsersByCustomer')
+            self.assertEqual(response[1], HTTPStatus.OK)
 
     # @patch.object(AuthService, 'list_users_by_role')
     # def test_get_users_by_role_success(self, mock_list_users_by_role):
